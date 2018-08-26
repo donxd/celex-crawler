2a37aaa788caaa25017206a29f6f453efd2d5315
donxd <n0s3.xd@gmail.com>
2018-08-26 00:15:57 -0500

    feat(server3): add methods for config parameters

--- server3.js
+++ server3.js
@@ -2,54 +2,75 @@ let schedule = require( 'node-schedule' );
 let request = require( 'request' );
 let cheerio = require( 'cheerio' );
 
-let numberRequests = 3;
+let numberRequests = getParameterNumberRequest();
+let fullProcess = getParamterFullProcess();
+
 let courseList = [];
 let requestProcessed = 0;
-const URL_DATA = 'https://celexupiicsa.info/?s=ubicación -examen&feed=rss2';
+
+// const URL_DATA = 'https://celexupiicsa.info/?s=ubicación -examen&feed=rss2';
+const URL_DATA_ALL = 'https://celexupiicsa.info/?s=ubicaci%C3%B3n%20-examen&feed=rss2'; // all data
+const URL_DATA_LINKS = 'https://celexupiicsa.info/?s=ubicaci%C3%B3n%20-examen&feed=rss'; // links
 
 for ( let i = 1; i <= numberRequests; i++ ){
 	makeRequest( i );
+}
+
+function getParameterNumberRequest (){
+	if (process.env.LIMIT_PAGE && !Number.isNaN(Number.parseInt(process.env.LIMIT_PAGE))) return Number.parseInt(process.env.LIMIT_PAGE);
+
+	return 1;
+}
+
+function getParamterFullProcess (){
+	if (process.env.FULL_PROCESS) return process.env.FULL_PROCESS === 'y';
+
+	return true;
 }
 
 function makeRequest ( pagination ){
 	let urlPagination = getUrlWithPagination( pagination );
 	console.log( 'mensaje - realizando petición [ %s ] ', urlPagination );
 
-	request( urlPagination, ( error, response, body ) => {
-		processBodyResponse( body );
+	request( urlPagination, ( error, response, content ) => {
+		processXMLResponse( content );
 	});
 }
 
 function getUrlWithPagination ( pagination ){
+	const urlData = fullProcess ? URL_DATA_ALL : URL_DATA_LINKS;
 	if ( pagination > 1 ){
-		return URL_DATA + '&paged=' + pagination;
+		return urlData + '&paged=' + pagination;
 	}
 
-	return URL_DATA;
+	return urlData;
 }
 
-function processBodyResponse ( body ){
-	let $ = cheerio.load( body );
-	let content = $( 'h1.entry-title' );
+function processXMLResponse ( content ){
+	let $ = cheerio.load( content );
 
-	if ( requestProcessed == 0 ){
-		courseList = [];
-	}
+	console.log('content request -> ', content);
 
-	content.each( ( index, element ) => {
-		// console.log( 'title [ %d ] -> %s ', index, $(element).text() );
+	// let content = $( 'h1.entry-title' );
 
-		if ( $( element ).text().indexOf( 'Listas de cursos de ') != -1 ){
-			courseList.push( $( element ) );
-		}
+	// if ( requestProcessed == 0 ){
+	// 	courseList = [];
+	// }
 
-	});
+	// content.each( ( index, element ) => {
+	// 	// console.log( 'title [ %d ] -> %s ', index, $(element).text() );
 
-	requestProcessed++;
+	// 	if ( $( element ).text().indexOf( 'Listas de cursos de ') != -1 ){
+	// 		courseList.push( $( element ) );
+	// 	}
 
-	if ( requestProcessed == numberRequests ){
-		printInformation( $ );
-	}
+	// });
+
+	// requestProcessed++;
+
+	// if ( requestProcessed == numberRequests ){
+	// 	printInformation( $ );
+	// }
 
 }
 
