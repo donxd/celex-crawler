db2a44d3f01bdb2ee3a02bc50d7bd9219e22059b
donxd <n0s3.xd@gmail.com>
2018-09-01 19:29:40 -0500

    feat(server3): change data to order

--- server3.js
+++ server3.js
@@ -154,7 +154,7 @@ function printInformation (){
 function showInformation ( dataCourses ){
 	dataCourses.forEach(course => {
 		// console.log('course -> ', JSON.stringify(course));
-		console.log(`course [ ${course.publication} ][ ${course.language} ][ ${course.level} ][ ${course.schedule} ][ ${course.semester} ][ ${course.teacher} ][ ${course.classroom} ][ s: ${course.students.length} ] `);
+		console.log(`course [ ${course.language} ][ ${course.level} ][ ${course.schedule} ][ ${course.publication} ][ ${course.semester} ][ ${course.teacher} ][ ${course.classroom} ][ s: ${course.students.length} ] `);
 		// course.students.forEach(student => {
 		// 	console.log( `student -> ${student} `);
 		// });
