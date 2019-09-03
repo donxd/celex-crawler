f12968b4e8f0e1e921e642fb3b0438b044f08909
donxd <n0s3.xd@gmail.com>
2019-09-03 18:43:24 -0500

    feat(server3): add persistence instance

--- server3.js
+++ server3.js
@@ -1,13 +1,15 @@
 const CelexData = require('./celex-data');
+const Persistence = require('./persistence');
+
 const data = new CelexData();
+const persistence = new Persistence();
 
 let numberRequests = data.getParameterNumberRequest();
 
 for ( let i = 1; i <= numberRequests; i++ ){
-	// new Persistence().showMessage();
 	data.makeRequest( i );
 }
 
-data.on('data-processed', () => {
-	console.log('data processed - OK');
+data.on('data-processed', dataCourses => {
+	persistence.processData( dataCourses );
 });
